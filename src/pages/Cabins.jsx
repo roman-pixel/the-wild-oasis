04d8b5ac05a0d01f7bcf49bdf7cb396261bbdfca
roman-pixel <romansrgvch@outlook.com>
2024-06-13 12:07:34 +0300

    add filters into cabins table

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,13 +3,14 @@ import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinsOperations from "../features/cabins/CabinsOperations";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinsOperations />
       </Row>
 
       <Row>
